04dff81c05ea4dcef375389cd9c92b7a73d3f937
Vihan Lakshman <vihanlaks@gmail.com>
2025-04-27 00:54:32 +0000

    Update functional embeddings script

--- functional_embeddings.py
+++ functional_embeddings.py
@@ -5,6 +5,8 @@ import numpy as np
 import pandas as pd
 import torch
 import torch.nn as nn
+
+from tqdm import tqdm
 from umap import UMAP
 
 from esm.models.esm3 import ESM3
@@ -12,6 +14,7 @@ from esm.sdk.api import ESM3InferenceClient, ESMProtein, GenerationConfig
 from esm.tokenization import InterProQuantizedTokenizer
 from esm.utils.types import FunctionAnnotation
 from huggingface_hub import login
+
 
 GO_TERM_TO_INTERPRO_IDS = {'GO:0009055': [], 
                            'GO:0008270': [], 
@@ -23,6 +26,18 @@ GO_TERM_TO_NAMES = {'GO:0009055': 'electron transfer activity',
                      'GO:0005179': 'hormone activity',
                      'GO:0003677': 'DNA binding'
                     }
+
+def read_interpro_data():
+    df = pd.read_csv('data/InterProDescriptions.tsv', sep='\t')
+    # Iterate through the DataFrame rows
+    for index, row in df.iterrows():
+        go_terms = str(row['GO Terms']).split(',')
+        for go_term in go_terms:
+            if go_term in GO_TERM_TO_INTERPRO_IDS:
+                GO_TERM_TO_INTERPRO_IDS[go_term].append(row['Accession'])
+
+    interpro_function_annotations = [FunctionAnnotation(label="IPR011992", start=1, end=1),]
+    return interpro_function_annotations
 
 
 def get_keywords_from_interpro(
@@ -71,73 +86,25 @@ def get_label_embedding(model, interpro_label, sequence):
         return None
 
 
-def main():
-    model = ESM3.from_pretrained("esm3_sm_open_v1").to("cuda") # or "cpu"
-    
-    df = pd.read_csv('data/InterProDescriptions.tsv', sep='\t')
-    # Iterate through the DataFrame rows
-    for index, row in df.iterrows():
-        go_terms = str(row['GO Terms']).split(',')
-        for go_term in go_terms:
-            if go_term in GO_TERM_TO_INTERPRO_IDS:
-                GO_TERM_TO_INTERPRO_IDS[go_term].append(row['Accession'])
-
-    interpro_function_annotations = [FunctionAnnotation(label="IPR011992", start=1, end=1),]
-
-    protein2 = ESMProtein(function_annotations=get_keywords_from_interpro(interpro_function_annotations))
-    
-    # List of standard amino acid single-letter codes
-    amino_acids = "ACDEFGHIKLMNPQRSTVWY"
-
-    # Function to generate a random amino acid sequence of a given length
-
-    def generate_random_sequence(length=10):
-        return ''.join(random.choices(amino_acids, k=length))
-
-    # List of sequences to generate embeddings for
-    sequences = ["A", "AAAAAAAAAA", "G", "GGGGGGGGGG","random"]
-
+def embed_go_terms(model, sequences):
     # Dictionary to store embeddings for all sequences by GO term
     go_term_to_embeddings = {}
-    i = 0
 
     # Process GO terms for each sequence
     for go_term, interpro_ids in GO_TERM_TO_INTERPRO_IDS.items():
         embeddings_by_sequence = {seq: [] for seq in sequences}
-        for interpro_id in interpro_ids:
+        for idx, interpro_id in tqdm(enumerate(interpro_ids)):
             for seq in sequences:
-                if seq=="random":
-                    actual_seq = generate_random_sequence()
-                else:
-                    actual_seq = seq
-
-                embedding = get_label_embedding(model, interpro_id, sequence=actual_seq)
-                if embedding is not None:
+                embedding = get_label_embedding(model, interpro_id, sequence=seq)
+                if embedding is not None and not np.allclose(embedding, 0):
                     embeddings_by_sequence[seq].append(embedding)
             
-            if i % 100 == 0:
-                print(f"Processed {i} GO terms")
-            i += 1
-        
-    go_term_to_embeddings[go_term] = embeddings_by_sequence
+        go_term_to_embeddings[go_term] = embeddings_by_sequence
     
-    # Create a copy of the original dictionary
-    original_go_term_to_embeddings = go_term_to_embeddings.copy()
+    return go_term_to_embeddings
 
-    # Iterate through the dictionary and remove all-zero arrays
-    new_go_term_to_embeddings = {}
-    for go_term, embeddings_by_sequence in go_term_to_embeddings.items():
-        new_embeddings_by_sequence = {}
-        for sequence, embeddings in embeddings_by_sequence.items():
-            new_embeddings = []
-            for embedding in embeddings:
-                if embedding is not None and not np.allclose(embedding, 0):
-                    new_embeddings.append(embedding)
-            new_embeddings_by_sequence[sequence] = new_embeddings
-    new_go_term_to_embeddings[go_term] = new_embeddings_by_sequence
 
-    go_term_to_embeddings = new_go_term_to_embeddings
-    
+def plot_results(sequences, go_term_to_embeddings):
     for seq in sequences:
         all_embeddings = []
         go_terms_for_embeddings = []
@@ -166,6 +133,18 @@ def main():
             plt.legend()
             plt.title(f"UMAP of ESM3 Embeddings with Sequence '{seq}' Colored by GO Term")
             plt.savefig(f"umap_{seq}.png")
+
+
+def main():
+    model = ESM3.from_pretrained("esm3_sm_open_v1").to("cuda") # or "cpu"
+    interpro_function_annotations = read_interpro_data()
+    
+    # List of sequences to generate embeddings for
+    sequences = ["A", "AAAAAAAAAA", "G", "GGGGGGGGGG"]
+    
+    go_term_to_embeddings = embed_go_terms(model, sequences)
+    plot_results(sequences, go_term_to_embeddings)
+
 
 if __name__ == "__main__":
     main()
