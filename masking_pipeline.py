c6dea4f5704816220d52b202370e95b9b2836f7a
Vihan Lakshman <vihanlaks@gmail.com>
2025-05-27 03:23:28 +0000

    Refactor masking pipeline script

--- masking_pipeline.py
+++ masking_pipeline.py
@@ -14,158 +14,80 @@ from esm.tokenization import InterProQuantizedTokenizer
 from esm.utils.types import FunctionAnnotation
 from huggingface_hub import login
 
-def get_keywords_from_interpro(
-    interpro_annotations,
-    interpro2keywords=InterProQuantizedTokenizer().interpro2keywords,
-):
-    keyword_annotations_list = []
-    for interpro_annotation in interpro_annotations:
-        keywords = interpro2keywords.get(interpro_annotation.label, [])
-        keyword_annotations_list.extend([
-            FunctionAnnotation(
-                label=keyword,
-                start=interpro_annotation.start,
-                end=interpro_annotation.end,
-            )
-            for keyword in keywords
-        ])
-    return keyword_annotations_list
+from utils import get_keywords_from_interpro
+
+def mask_sequence(model, df, df2, embeddings_dict, selected_proteins=None):
+    matrix = bl.BLOSUM(80)
+    allnuminterpro = []
+    allpercentmasks = df['percent_deleted'].tolist()
+    allpercentidentities = []
+    allindexes = []
+    allmasked = []
+    sequence_similarity = []
+    masked_sequence = []
+    generated_sequence_list = []
+    protein_embedding_list = []
+    
+    df_to_iterate = selected_proteins if selected_proteins is not None else df
+
+    for index, row in df_to_iterate.iterrows():
+        if row["Entry"] not in embeddings_dict:
+            continue
+        if index not in df2['Index'].tolist():
+            continue
+        else:
+            modified_prompt = df2[df2['Index']==index]['Masked Sequences'].tolist()[0]
+      
+        protein_prompt = ESMProtein(sequence=modified_prompt)
+
+        # make the function annotations
+        interpro_ids = embeddings_dict[df.iloc[index]['Entry']]
+        functionlist = []
+        for interpro_id in interpro_ids['InterPro_ids']:
+            functionlist.append(FunctionAnnotation(label=interpro_id, start=1, end=len(modified_prompt)))
+
+        # generate w/function annotations
+        protein_prompt.function_annotations = get_keywords_from_interpro(functionlist)
+        torch.cuda.empty_cache()
+        sequence_generation = model.generate(
+              protein_prompt,
+              GenerationConfig(
+              track="sequence",
+              num_steps=protein_prompt.sequence.count("_") // 2,
+              temperature=0.5,
+          ),
+        )
+
+        generated_sequence = sequence_generation.sequence
+        generated_sequence_list.append(generated_sequence)
+        # Ensure sequences are of equal length
+        if len(generated_sequence) != len(row['sequence']):
+            print("Sequences must be of the same length to calculate Hamming distance.")
+            sequence_similarity.append(None)
+        else:
+            blosum_score = 0
+            for gen_residue, target_residue in zip(generated_sequence, row['sequence']):
+                blosum_val =  matrix[gen_residue][target_residue]
+                blosum_score += blosum_val
+            blosum_score = blosum_score / len(generated_sequence)
+            sequence_similarity.append(blosum_score)
+        torch.cuda.empty_cache()
+
+    return generated_sequence_list, sequence_similarity
 
 
-def get_label_embedding(interpro_label,sequence):
-  hostProtein = ESMProtein(sequence=sequence)
-  embedding_function = model.encoder.function_embed
-  hostProtein.function_annotations = get_keywords_from_interpro([FunctionAnnotation(label=interpro_label, start=1, end=len(sequence))])
-  hostProtein_tensor = model.encode(hostProtein)
-  device = hostProtein_tensor.function.device  # Get the device of protein2_tensor.function
-  embedding_function = embedding_function.to(device)  # Move embedding_function to the device
+def run_masking_pipeline(model, df, df2, df3, embeddings_dict):
+    matrix = bl.BLOSUM(80)
+    generated_sequence_list, sequence_similarity = mask_sequence(model, df, df2, embeddings_dict)
+    df2['Generated Sequences'] = generated_sequence_list
+    df2['Sequence Similarity'] = sequence_similarity
+    selected_proteins = df[df.index.isin(df2['Index'])] 
+    generated_sequence_list, sequence_similarity = mask_sequence(model, df, df3, embeddings_dict, selected_proteins=selected_proteins)
+    
+    df3['Generated Sequences'] = generated_sequence_list
+    df3['Sequence Similarity'] = sequence_similarity
+    return df2, df3
 
-  function_embed = torch.cat(
-      [
-          embed_fn(funcs.to(device)) # Ensure funcs is on the same device
-          for embed_fn, funcs in zip(
-              embedding_function, hostProtein_tensor.function.unbind(-1)
-          )
-      ],
-      -1,
-  )
-  if function_embed.shape[0] >= 3:
-      row_sum = function_embed.sum(dim=0)  # Sum all rows
-      row_avg = row_sum / (function_embed.shape[0] - 2)  # Divide by (number of rows - 2)
-      row_avg_np = row_avg.cpu().detach().type(torch.float32).numpy()
-      return row_avg_np
-  else:
-      return None
-
-
-
-def calculatesquareddist(proteinembed, embedding_np):
-    # Ensure inputs are numpy arrays for compatibility with cosine_similarity
-    proteinembed = np.array(proteinembed)
-    embedding_np = np.array(embedding_np)
-
-    squared_similarities = []
-
-    # Iterate over each row in proteinembed
-    for protein_row in proteinembed:
-        # Compute cosine similarity of the current row with all rows of embedding_np
-        cos_sim = cosine_similarity(embedding_np, protein_row.reshape(1, -1)) ** 2
-        # Sum the squared cosine similarities
-        squared_sum = np.sum(cos_sim)
-        squared_similarities.append(squared_sum)
-
-    # Convert results to a tensor for consistency if needed
-    return squared_similarities
-
-
-def embedding_masking_model(
-    prompt,
-    model,
-    df,
-    embeddings_dict,
-    percentage=10,
-):
-    """
-    Helper function to process a protein sequence, calculate similarities,
-    and return indices for masking.
-
-    Args:
-        prompt (str): The protein sequence to be processed.
-        model: The model used for protein generation and embeddings.
-        df (pd.DataFrame): DataFrame containing protein data.
-        embeddings_dict (dict): Dictionary storing embeddings and other details.
-
-    Returns:
-        List[int]: Indices used for masking in the sequence.
-    """
-    # Create an ESMProtein object
-    protein = ESMProtein(sequence=prompt)
-
-    # Configure the model for generation
-    generation_config = GenerationConfig(track="function", num_steps=8)
-
-    # Generate the protein
-    generated_protein = model.generate(protein, generation_config)
-
-    # Check if function annotations are available
-    entry = df.loc[df['substituted_seq'] == prompt, 'Entry'].iloc[0]
-    if generated_protein.function_annotations is None:
-        embeddings_dict[entry]['hamming_distance'] = None
-        return [],[]
-
-    # Getting embedding for the protein
-    protein_tensor = model.encode(generated_protein)
-    embedding_function = model.encoder.function_embed
-    device = protein_tensor.function.device  # Get the device of protein_tensor.function
-    embedding_function = embedding_function.to(device)  # Move embedding_function to the device
-
-    function_embed = torch.cat(
-        [
-            embed_fn(funcs.to(device))  # Ensure funcs is on the same device
-            for embed_fn, funcs in zip(
-                embedding_function, protein_tensor.function.unbind(-1)
-            )
-        ],
-        -1,
-    )
-
-    # Exclude start and end tokens
-    function_embed = function_embed[1:-1, :]
-
-    # Convert the protein_tensor.function to a NumPy array
-    protein_np = function_embed.cpu().detach().type(torch.float32).numpy()
-
-    # Retrieve target sequence and embedding
-    embedding = embeddings_dict[entry]['embedding']
-
-    # Calculate cosine similarity
-    #embedding = embedding.cpu().detach().type(torch.float32).numpy()
-    similarities = calculatesquareddist(protein_np, embedding)
-
-    num_indices = int(len(prompt) * percentage / 100)
-
-    # Ensure we select at least 1 index
-    num_indices = max(1, num_indices)
-
-    # Find the top 10 indices with the lowest cosine similarity
-    indices = np.argsort(similarities)[:num_indices]
-
-    # Store the indices in the embeddings_dict
-    embeddings_dict[entry]['indices'] = indices.tolist()
-
-    return indices.tolist(), protein_np
-
-
-def get_random_indices(prompt, percentage):
-    """
-    Randomly select indices to mask based on the percentage of the prompt's length.
-    """
-    num_indices = int(len(prompt) * percentage / 100)
-    # Ensure we select at least one index
-    num_indices = max(1, num_indices)
-
-    # Randomly select unique indices to mask
-    return random.sample(range(len(prompt)), num_indices)
 
 def plot_results(df2, df3):
     # Fit regression lines
@@ -279,203 +201,18 @@ def plot_results(df2, df3):
     plt.grid(True)
     plt.show()
 
+
 def main():
     df = pd.read_csv('data/with_seq_similarity_and_mutant_seq_input_df_sumSquare.csv')
     df2 = pd.read_csv('data/with_seq_similarity_embedding_output_full_sumSquare.csv')
     df3 = pd.read_csv('data/with_seq_similarity_random_output_full_sumSquare.csv')
-    df2_full_exists = True
-    df3_full_exists = True
-    matrix = bl.BLOSUM(80)
 
     model = ESM3.from_pretrained("esm3_sm_open_v1").to('cuda')
     embeddings_dict = np.load('data/embeddings_dict.npy',allow_pickle=True)
     embeddings_dict = dict(embeddings_dict.item())
     
-    row = df.iloc[6]
-    maskedindeces,embedding = embedding_masking_model(row['substituted_seq'], model, df, embeddings_dict,percentage=math.ceil(row['percent_deleted']))
-
-
-    allnuminterpro = []
-    allpercentmasks = df['percent_deleted'].tolist()
-    allpercentidentities = []
-    allindexes = []
-    allmasked = []
-    sequence_similarity = []
-    masked_sequence = []
-    generated_sequence_list = []
-    protein_embedding_list = []
-    for index, row in df.iterrows():
-      if row["Entry"] not in embeddings_dict:
-        continue
-      if not df2_full_exists:
-        maskedindeces, protein_embedding = embedding_masking_model(row['substituted_seq'], model, df, embeddings_dict,percentage=math.ceil(row['percent_deleted']))
-        if not maskedindeces:
-          continue
-        protein_embedding_list.append(protein_embedding)
-        allindexes.append(index)
-        numinterpro = int(len(row['InterPro'])/10)
-        allnuminterpro.append(numinterpro)
-        correctmasks = set(np.arange(row['del_start'],row['del_end']+1))
-        truncatedpredictions = set(maskedindeces[:len(correctmasks)])
-        allmasked.append(truncatedpredictions)
-        identical_count = len(truncatedpredictions.intersection(correctmasks))
-        percent_identity = (identical_count / len(correctmasks))
-        allpercentidentities.append(percent_identity)
-        modified_prompt = list(row['substituted_seq'])
-        for index in maskedindeces:
-            modified_prompt[index] = "_"
-        modified_prompt = "".join(modified_prompt)
-        masked_sequence.append(modified_prompt)
-      else:
-        if index not in df2['Index'].tolist():
-          continue
-        else:
-          modified_prompt = df2[df2['Index']==index]['Masked Sequences'].tolist()[0]
-      protein_prompt = ESMProtein(sequence=modified_prompt)
-
-      #make the function annotations
-      interpro_ids = embeddings_dict[df.iloc[index]['Entry']]
-      functionlist = []
-      for interpro_id in interpro_ids['InterPro_ids']:
-        functionlist.append(FunctionAnnotation(label=interpro_id, start=1, end=len(modified_prompt)))
-
-
-      #generate w/function annotations
-      protein_prompt.function_annotations = get_keywords_from_interpro(functionlist)
-      torch.cuda.empty_cache()
-      sequence_generation = model.generate(
-              protein_prompt,
-              GenerationConfig(
-              track="sequence",
-              num_steps=protein_prompt.sequence.count("_") // 2,
-              temperature=0.5,
-          ),
-      )
-
-      generated_sequence = sequence_generation.sequence
-      generated_sequence_list.append(generated_sequence)
-      # Ensure sequences are of equal length
-      if len(generated_sequence) != len(row['sequence']):
-          print("Sequences must be of the same length to calculate Hamming distance.")
-          sequence_similarity.append(None)
-      else:
-          blosum_score = 0
-          for gen_residue, target_residue in zip(generated_sequence, row['sequence']):
-              blosum_val =  matrix[gen_residue][target_residue]
-              blosum_score += blosum_val
-          blosum_score = blosum_score / len(generated_sequence)
-          sequence_similarity.append(blosum_score)
-      torch.cuda.empty_cache()
-
-    if not df2_full_exists:
-      shortenedpercentmasks = [allpercentmasks[i] for i in allindexes]
-      df2 = pd.DataFrame({
-          'Number of Interpro Terms': allnuminterpro,
-          'Percentage Deleted': shortenedpercentmasks,
-          'Percent Correct': allpercentidentities,
-          'Index': allindexes,
-          'Masked sites': allmasked,
-          'Sequence Similarity': sequence_similarity,
-          'Generated Sequences': generated_sequence_list,
-          'Masked Sequences': masked_sequence
-      })
-    else:
-      df2['Generated Sequences'] = generated_sequence_list
-      df2['Sequence Similarity'] = sequence_similarity
-
-    # Save the DataFrame as a CSV file
-    df2.to_csv('with_seq_similarity_embedding_output_full.csv')
-
-    allnuminterpro = []
-    allpercentmasks = df['percent_deleted'].tolist()
-    allpercentidentities = []
-    allindexes = []
-    allmasked = []
-    sequence_similarity = []
-    masked_sequence = []
-    generated_sequence_list = []
-    selected_proteins = df[df.index.isin(df2['Index'])]
-    for index,row in selected_proteins.iterrows():
-      if row["Entry"] not in embeddings_dict:
-        continue
-      if not df3_full_exists:
-        maskedindeces = get_random_indices(row['substituted_seq'], percentage=math.ceil(row['percent_deleted']))
-        if not maskedindeces:
-          continue
-        allindexes.append(index)
-        numinterpro = int(len(row['InterPro'])/10)
-        allnuminterpro.append(numinterpro)
-        correctmasks = set(np.arange(row['del_start'],row['del_end']+1))
-        truncatedpredictions = set(maskedindeces[:len(correctmasks)])
-        allmasked.append(truncatedpredictions)
-        identical_count = len(truncatedpredictions.intersection(correctmasks))
-        percent_identity = (identical_count / len(correctmasks))
-        allpercentidentities.append(percent_identity)
-        modified_prompt = list(row['substituted_seq'])
-        for index in maskedindeces:
-            modified_prompt[index] = "_"
-        modified_prompt = "".join(modified_prompt)
-        masked_sequence.append(modified_prompt)
-      else:
-        if index not in df3['Index'].tolist():
-          continue
-        else:
-          modified_prompt = df3[df3['Index']==index]['Masked Sequences'].tolist()[0]
-      protein_prompt = ESMProtein(sequence=modified_prompt)
-      #make the function annotations
-      interpro_ids = embeddings_dict[df.iloc[index]['Entry']]
-      functionlist = []
-      for interpro_id in interpro_ids['InterPro_ids']:
-        functionlist.append(FunctionAnnotation(label=interpro_id, start=1, end=len(modified_prompt)))
-
-
-      #generate w/function annotations
-      protein_prompt.function_annotations = get_keywords_from_interpro(functionlist)
-      sequence_generation = model.generate(
-          protein_prompt,
-          GenerationConfig(
-              track="sequence",
-              num_steps=protein_prompt.sequence.count("_") // 2,
-              temperature=0.5,
-          ),
-      )
-      generated_sequence = sequence_generation.sequence
-      generated_sequence_list.append(generated_sequence)
-      # Ensure sequences are of equal length
-      if len(generated_sequence) != len(row['sequence']):
-          print("Sequences must be of the same length to calculate Hamming distance.")
-          sequence_similarity.append(None)
-      else:
-          blosum_score = 0
-          for gen_residue, target_residue in zip(generated_sequence, row['sequence']):
-              blosum_val =  matrix[gen_residue][target_residue]
-              blosum_score += blosum_val
-          blosum_score = blosum_score / len(generated_sequence)
-          sequence_similarity.append(blosum_score)
-
-      torch.cuda.empty_cache()
-
-    if not df3_full_exists:
-      shortenedpercentmasks = [allpercentmasks[i] for i in allindexes]
-      df3 = pd.DataFrame({
-          'Number of Interpro Terms': allnuminterpro,
-          'Percentage Deleted': shortenedpercentmasks,
-          'Percent Correct': allpercentidentities,
-          'Index': allindexes,
-          'Masked sites': allmasked,
-          'Sequence Similarity': sequence_similarity,
-          'Generated Sequences': generated_sequence_list,
-          'Masked Sequences': masked_sequence
-      })
-    else:
-      df3['Generated Sequences'] = generated_sequence_list
-      df3['Sequence Similarity'] = sequence_similarity
-
-
-    df3.to_csv('with_seq_similarity_random_output_full.csv')
-
+    df2, df3 = run_masking_pipeline(model, df, df2, df3, embeddings_dict)
     plot_results(df2, df3)
-
 
 
 if __name__=='__main__':
